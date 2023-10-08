206bebff385656a29d1331954471c6d1f6f773e2
Ángel Mérida <mer23661@uvg.edu.gt>
2023-10-08 15:18:27 -0600

    Clase producto
    primer

---
+++ Producto.java
@@ -0,0 +1,66 @@
+public class Producto {
+    private int id;
+    private String nombre;
+    private float precio;
+    private int cantidad;
+    private String descripcion;
+    private int idVendedor;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        if (id < 0){
+            return;
+        }
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        if (precio < 0){
+            return;
+        }
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        if (cantidad < 0){
+            return;
+        }
+        this.cantidad = cantidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+}
