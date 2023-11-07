20ac104977bf037088be933ef540b8364e543b75
357U4RD0 <cas23890@uvg.edu.gt>
2023-11-07 13:51:01 -0600

    Importación paquetes

--- Server.java
+++ Server.java
@@ -1,10 +1,17 @@
 package Marketplace;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Clase para métodos que trabajan conexiones con el servidor
  * @author Grupo de Marketplace
  * @version 7-11-2023
  */
 public class Server {
+    private static Connection connection;
+
     //TODO: Añadir método para conectarse al servidor
 
     //TODO: Añadir método para consultar datos al servidor
