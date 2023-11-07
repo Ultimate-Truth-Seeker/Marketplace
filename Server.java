c858f9917cb82dc17d9418b87d5d20c34fb952e9
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2023-11-07 13:44:36 -0600

    asignación de tareas

--- Server.java
+++ Server.java
@@ -1,9 +1,18 @@
 package Marketplace;
 /**
- * Clase para métodos que trabajan con el servidor
+ * Clase para métodos que trabajan conexiones con el servidor
  * @author Grupo de Marketplace
  * @version 7-11-2023
  */
 public class Server {
+    //TODO: Añadir método para conectarse al servidor
+
+    //TODO: Añadir método para consultar datos al servidor
+
+    //TODO: Añadir método para añadir datos al servidor
     
+    //TODO: Añadir método para modificar datos al servidor
+    
+    //TODO: Añadir método para eliminar datos al servidor
+
 }
